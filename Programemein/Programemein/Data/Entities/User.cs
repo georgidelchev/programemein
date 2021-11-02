d487cc2e84b006feb217525615c68a0c0df98040
Viktor <swifrorlilko@gmail.com>
2021-11-02 23:22:14 +0300

    FIX: Add idenity to user

--- Programemein/Programemein/Data/Entities/User.cs
+++ Programemein/Programemein/Data/Entities/User.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Programemein.Data.Entities
 {
-    public class User
+    public class User : IdentityUser
     {
     }
 }
