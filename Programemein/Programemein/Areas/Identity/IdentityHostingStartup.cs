fca0b1833c04b2da7f263a3b9e0a64272b6ac269
george <alienguymc@gmail.com>
2021-11-03 00:44:49 +0200

    Scaffold login and register. Make login use UserName instead of Email for the login.

---
+++ Programemein/Programemein/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Programemein.Data;
+using Programemein.Data.Entities;
+
+[assembly: HostingStartup(typeof(Programemein.Areas.Identity.IdentityHostingStartup))]
+namespace Programemein.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
