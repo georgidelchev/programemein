b0c67f46ae4745e548348660af3a159a3112e94c
george <alienguymc@gmail.com>
2021-11-03 00:53:22 +0200

    Split Email and Username as differend fields during the registration process.

--- Programemein/Programemein/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Programemein/Programemein/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -52,6 +52,9 @@ namespace Programemein.Areas.Identity.Pages.Account
             public string Email { get; set; }
 
             [Required]
+            public string UserName { get; set; }
+
+            [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
@@ -75,7 +78,7 @@ namespace Programemein.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.UserName, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
