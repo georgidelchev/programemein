87922427130e69a9c930dfc81ea2bbd42fa15360
Viktor <swifrorlilko@gmail.com>
2021-11-02 22:13:11 +0300

    ADD: User entity

--- Programemein/Programemein/Data/ApplicationDbContext.cs
+++ Programemein/Programemein/Data/ApplicationDbContext.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Programemein.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Programemein.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
